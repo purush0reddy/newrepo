c21d89953f9eceb4444fa93996cf962ce90ee99b
purush <purush3190@gmail.com>
2021-01-31 10:57:50 +0530

    sucessfully

---
+++ 10 loditri.py
@@ -0,0 +1,10 @@
+def loydTri(n): 
+  
+    val = 1
+    for i in range(1, n + 1): 
+        for j in range(1, i + 1): 
+            print(val, end = " ") 
+            val += 1
+        print("") 
+  
+loydTri(6)
