8b4aca3d3b5d053fe921d5a98c06e7c06085d06f
purush <purush3190@gmail.com>
2021-02-01 08:01:09 +0530

    13

---
+++ 13.py
@@ -0,0 +1,10 @@
+"""
+Step 1 : Start.
+Start 2 : Read n.
+Start 3 : Initialize counter variable i to 1 and fact to 1.
+Start 4 : if i <= n go to step 5 otherwise goto step 7.
+Start 5 : calculate fact = fact * i.
+Start 6 : increment counter variable i and goto step 4.
+Start 7 : Write fact.
+Start 8 : Stop.
+"""
